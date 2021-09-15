666f624c7db31a82752cf9e70014e64919b77d29
AlirezaO <alirezaomidi2079@yahoo.com>
2021-09-16 02:49:22 +0430

    updating our shop page and styling items a bit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
-
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage =() => (
   <div>
@@ -15,6 +15,7 @@ function App() {
     <div >
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
         <Route exact path='/shop/hats' component={HatsPage}/>
       </Switch>
     </div>
