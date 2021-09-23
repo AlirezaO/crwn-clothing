74458bf287342f327f0229ee56513dde708955f3
AlirezaO <alirezaomidi2079@yahoo.com>
2021-09-23 17:15:47 +0330

    implemented firebase and added sign up ability

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,15 @@
 import react from "react";
 import './sign-in-and-sign-up.styles.scss';
 import SignIn  from "../../components/sing-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUp = () =>(
 
-    <div className='sign-in-and-sign-up'><SignIn/></div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+        <SignUp/>    
+    </div>
+    
 
 )
 
