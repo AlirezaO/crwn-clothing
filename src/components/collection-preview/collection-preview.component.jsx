385c01fee95487c300e3be15a78857857f7cca86
AlirezaO <alirezaomidi2079@yahoo.com>
2021-09-30 22:46:07 +0330

    updated cart dropdown and cart items in it.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,9 +10,9 @@ const CollectionPreview = ({id, title, items}) => (
         <div className='preview'>
             {items
             .filter((item, idx) => idx < 4 )
-            .map(({id, ...OtherItemProps}) => (
+            .map((item) => (
                 
-                <CollectionItem key={id} {...OtherItemProps}/>
+                <CollectionItem key={item.id} item={item}/>
                 
             ))}
         </div>
